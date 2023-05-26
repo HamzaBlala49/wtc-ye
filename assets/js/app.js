0a20afb3a5db30b0663964a70ed76f6008aa5e29
Hamza Omer Blala <hamzaomarblala@gmail.com>
2023-05-26 07:46:46 -0700

    7th

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 // selectors
-let preLoader = document.querySelector('.preloader')
+// let preLoader = document.querySelector('.preloader')
 let navbar =document.querySelector('.mynavbar')
 let linkNavbar =document.querySelectorAll('.mynavbar .links .link a')
 let linkSideBar =document.querySelectorAll('.sidebar .links .link a')
@@ -12,12 +12,12 @@ let social =document.querySelector('.social')
 
 
 // start preLoader
-window.addEventListener('load',()=>{
-    preLoader.style.opacity =0;
-    setTimeout(()=>{
-        preLoader.style.display ='none';
-    },2000)
-})
+// window.addEventListener('load',()=>{
+//     preLoader.style.opacity ='0';
+//     setTimeout(()=>{
+//         preLoader.style.display ='none';
+//     },2000)
+// })
 // end preLoader
 // start navbar
 window.addEventListener('scroll',()=>{
@@ -44,23 +44,6 @@ linkSideBar.forEach(el =>{
 })
 //end sideBar
 
-// start copy
-let date = new Date()
-copy.innerHTML = date.getFullYear()
-// end copy
 
-
-// start mode
-
-modeBtn.addEventListener('click',()=>{
-    if(!modeBtn.classList.contains('light')){
-        modeBtn.classList.add("light")
-        document.body.classList.add("dark")
-    }else{
-        modeBtn.classList.remove('light')
-        document.body.classList.remove("dark")
-        
-    }
-})
 
 // end mode
